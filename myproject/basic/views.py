f26daa05395ba095b020e4452d90bd0b5514fa6a
Naveen Kumar <your_email@example.com>
2025-11-07 00:45:30 +0530

    first commit

---
+++ myproject/basic/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.http import HttpResponse 
+from django.http import JsonResponse
+# Create your views here.
+
+def sample(requst):
+    return HttpResponse("Hello World! Django is working fine 😊")
+
+def sampleinfo(request):
+    data = { "name": "naveen","age":22,"city":"hyd"}
+    return JsonResponse(data)
+
+def dynamicResponce(request):
+    name=request.GET.get("name", ' ')
+    return HttpResponse(f"hello{name}")
+
+
+def calculate(requst):
+    a=10
+    b=20
+    addition = a + b
+    subtraction = a - b
+    multiplication = a * b
+    division = b / a
+
+    result = f""" <h1> The calculation between a and b </h1> <br>
+       given a value:{a} <br> given b value: {b} <br>
+       additon: {addition} <br>
+       subtraction: {subtraction} <br>
+       multiplication: {multiplication} <br>
+       division:  {division} """
+    
+    return HttpResponse(result)
+
+    
